b3aa4c83810b5fccfd1df9a9de63a78872d66c5c
Md Khan <khan4019@gmail.com>
2015-02-08 23:12:23 -0600

    added files for first angular Perf

---
+++ demo/console.js
@@ -0,0 +1,129 @@
+//1.log multiple items
+
+var first = 225;
+var second = 100;
+var third = 55;
+
+
+
+
+//2. log multiple objects
+
+var jsDude = { name: 'that js dude',
+					favLib: 'herald washington library',
+					favDrink: 'water'
+				};
+
+var awesomeDude = { name: 'Mr. awesome',
+					favLib: 'awesome library',
+					favDrink: 'beer'
+				};
+
+var regularDude = { name: 'Who cares',
+					favLib: 'hates reading',
+					favDrink: 'meetup beer'
+				};
+
+
+
+
+//3. log array of objects
+
+var arrOfObjects = [jsDude, awesomeDude, regularDude];
+
+
+
+//4. array of arrays
+var myArrays = [[5,7,8], [11,5,8], [7,5,6], [2,99,45,3], [333,234]];
+
+
+
+
+//5. get data from server
+//output from some ajax call
+var data = javaScriptDevelopersDataFromServer;
+
+
+
+
+//6. see all properties of an object
+
+var myObj = {
+	name: 'just an object',
+	duty: 'do nothing',
+	child: {
+		name: 'son',
+		age:'can walk',
+		grandChild: {
+			name: 'grand child',
+			age: -17
+		}
+	}
+}
+
+
+
+
+
+
+//7. presever log
+
+
+
+
+//8. clear console
+
+
+
+//9. filter, log xhr
+
+
+
+//10. different types of log
+//info, warn, error, debug
+// console.log('simple log');
+// console.debug('its an debug');
+// console.info('its info');
+// console.warn('be careful. i am warning you');
+// console.error('something went wrong');
+
+
+//11.kesy
+var aboutJavascript = {
+	name: 'javaScript',
+	parts: ['function', 'object', 'closure', 'prototype'],
+	age: 19,
+	birthYear: 1995,
+	father: 'Brendan Eich',
+	grades: 6,
+	rank:1,
+	numberOfLibraries: Infinity
+}
+
+
+//12.values
+
+
+
+
+
+
+
+//11. count number of call to a function
+function add(a, b){
+	return a + b;
+}
+
+//12. group
+var total = 0;
+
+for (var i = 0; i < 100; i++) {
+
+	total += i;
+};
+
+
+//13. 
+
+//13. add this. http://stackoverflow.com/questions/10224691/output-multi-dimensional-arrays
+
